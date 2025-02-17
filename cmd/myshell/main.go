58386106ef133bdc4f68a503f58a5dec2a124d21
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-02-17 19:49:45 +0330

    hey

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
-
 
 func main() {
 	for {
@@ -15,9 +16,15 @@ func main() {
 			fmt.Fprint(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		if command[0] == '\n' {
+		splited := strings.Split(command, " ")
+		parent := splited[0]
+		switch parent {
+		case "\n":
 			os.Exit(0)
+		case "exit":
+			exitCode, _ := strconv.Atoi(strings.TrimSpace(splited[1]))
+			os.Exit(exitCode)
 		}
-		fmt.Fprintln(os.Stdout, command[:len(command)-1] + ": command not found")
+		fmt.Fprintln(os.Stdout, command[:len(command)-1]+": command not found")
 	}
 }
