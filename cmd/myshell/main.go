12eead76019791a1e3706266057d7116c4eecd7a
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-02-17 19:36:32 +0330

    hey

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,19 +6,18 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-
-	if err != nil {
-		fmt.Fprint(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		if command[0] == '\n' {
+			os.Exit(0)
+		}
+		fmt.Fprintln(os.Stdout, command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Fprintln(os.Stdout, command[:len(command)-1] + ": command not found")
 }
