2b77a23372a96fd97e9a5784f0962a579f6bcae1
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2025-02-19 16:16:26 +0330

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,15 +13,19 @@ import (
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
+		
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			panic(err)
+
 		}
 		full_command := strings.Fields(command)
 		if len(full_command) == 0 {
 			os.Exit(0)
 		}
 		parent := full_command[0]
+		args := full_command[1:]
+		
 		switch parent {
 
 		case "type":
@@ -30,12 +34,19 @@ func main() {
 		case "exit":
 			exit(full_command[1])
 
-		case "echo":
-			echo(full_command[1:])
-
 		default:
-			fmt.Fprintln(os.Stdout, strings.TrimSpace(command)+": command not found")
+			run(parent, args)
 		}
+	}
+}
+
+func run(command string, args []string) {
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(strings.TrimSpace(command)+": command not found")	
 	}
 }
 
@@ -56,16 +67,4 @@ func type_command(command string) {
 func exit(code string) {
 	exitCode, _ := strconv.Atoi(strings.TrimSpace(code))
 	os.Exit(exitCode)
-}
-
-func echo(args []string) {
-	n := len(args)
-	for i := 0; i < n; i++ {
-		if i < n-1 {
-			fmt.Print(args[i] + " ")
-		} else {
-			fmt.Print(args[i])
-		}
-	}
-	fmt.Println()
 }
